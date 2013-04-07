37fe7cafddef50ac196c31222fceee9dff193ebe
Thomas Levine <occurrence@thomaslevine.com>
2013-04-07 18:20:42 -0400

    oops

--- data/rows-download.sh
+++ data/rows-download.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir -p downloads/rows
 for viewid in $(./datasets-parse.py); do
-  test -e "downloads/rows/${viewid}.csv" || curl "https://data.cityofnewyork.us/api/views/${viewid}.csv?accessType=DOWNLOAD" > "downloads/rows/${viewid}.csv"
+  test -e "downloads/rows/${viewid}.csv" || curl "https://data.cityofnewyork.us/api/views/${viewid}/rows.csv?accessType=DOWNLOAD" > "downloads/rows/${viewid}.csv"
 done
