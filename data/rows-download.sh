87967e86479e13def2ad229f46f0abba1229873d
Thomas Levine <occurrence@thomaslevine.com>
2013-04-07 22:10:51 -0400

    null output

--- data/rows-download.sh
+++ data/rows-download.sh
@@ -3,6 +3,6 @@ set -e
 
 mkdir -p downloads/rows
 for viewid in $(./datasets-parse.py); do
-  grep 311\ Service\ Requests "downloads/views/${viewid}.json" && continue
+  grep 311\ Service\ Requests "downloads/views/${viewid}.json" > /dev/null && continue
   test -e "downloads/rows/${viewid}.csv" || curl "https://data.cityofnewyork.us/api/views/${viewid}/rows.csv?accessType=DOWNLOAD" > "downloads/rows/${viewid}.csv"
 done
